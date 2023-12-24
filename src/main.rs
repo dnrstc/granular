e3967c61928f329e11ad4d3ad052e4553c201c8f
dnrstc <danrstc@gmail.com>
2023-12-24 18:30:26 +0100

    Parse files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{Context, Result};
+
+fn read_file(path: &str) -> color_eyre::Result<String> {
+    let contents = std::fs::read_to_string(path).wrap_err("reading {path}")?;
+    Ok(contents)
+}
 
 fn main() -> Result<()> {
-    println!("Hello, Granular!");
+    color_eyre::install()?;
+
+    let html = read_file("index.html")?;
+    let css = read_file("style.css")?;
+
+    println!("{html}");
+    println!("{css}");
 
     Ok(())
 }
