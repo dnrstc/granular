38d0f7115ec86eceacd2b77f029133125b94a74f
dnrstc <danrstc@gmail.com>
2023-12-24 18:54:58 +0100

    Parse cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
+use clap::Parser;
 use color_eyre::eyre::{Context, Result};
 
+#[derive(Parser)]
+struct Cli {
+    html_path: std::path::PathBuf,
+    css_path: std::path::PathBuf,
+}
+
 fn read_file(path: &str) -> color_eyre::Result<String> {
-    let contents = std::fs::read_to_string(path).wrap_err("reading {path}")?;
+    let contents = std::fs::read_to_string(path).wrap_err_with(|| format!("reading {path}"))?;
     Ok(contents)
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let html = read_file("index.html")?;
-    let css = read_file("style.css")?;
+    let Cli {
+        css_path,
+        html_path,
+    } = Cli::parse();
+
+    let html_path = html_path.to_str().unwrap();
+    let css_path = css_path.to_str().unwrap();
+
+    let html = read_file(html_path)?;
+    let css = read_file(css_path)?;
 
     println!("{html}");
     println!("{css}");
