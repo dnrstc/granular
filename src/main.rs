3bd76bb4d3c820aed654a357b10d356fbcba9b37
dnrstc <danrstc@gmail.com>
2023-12-25 17:40:34 +0100

    Parse css contents to CssObject struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 use clap::Parser;
 use color_eyre::eyre::{Context, Result};
 
@@ -7,9 +9,64 @@ struct Cli {
     css_path: std::path::PathBuf,
 }
 
+#[derive(Debug)]
+struct CssObject {
+    selector: String,
+    rules: Vec<String>,
+}
+
 fn read_file(path: &str) -> color_eyre::Result<String> {
     let contents = std::fs::read_to_string(path).wrap_err_with(|| format!("reading {path}"))?;
     Ok(contents)
+}
+
+fn parse_classes(html_contents: &str) -> Result<()> {
+    let mut classes: Vec<&str> = vec![];
+
+    for line in html_contents.lines() {
+        let class = line.split_once("class=");
+        match class {
+            Some((_, class_name)) => {
+                let new_class_name = class_name.split("\"").collect::<Vec<_>>()[1];
+                classes.push(new_class_name);
+            }
+            None => (),
+        }
+    }
+
+    println!("{classes:?}");
+
+    Ok(())
+}
+
+fn parse_css(css_contents: &str) -> Result<()> {
+    let mut styles = Vec::new();
+    let mut selector = String::new();
+    let mut rules = Vec::new();
+
+    for line in css_contents.lines() {
+        if line.starts_with('.') {
+            selector = line.split('{').next().unwrap().trim().to_string();
+        } else if !line.contains('}') {
+            if !line.trim().is_empty() {
+                rules.push(line.trim().to_string());
+            }
+        } else if line.contains('}') {
+            let css_object = CssObject {
+                selector: selector.clone(),
+                rules: rules.clone(),
+            };
+
+            selector.clear();
+            rules.clear();
+
+            styles.push(css_object);
+        }
+    }
+
+    println!("{:?}", styles);
+
+    Ok(())
 }
 
 fn main() -> Result<()> {
@@ -26,8 +83,8 @@ fn main() -> Result<()> {
     let html = read_file(html_path)?;
     let css = read_file(css_path)?;
 
-    println!("{html}");
-    println!("{css}");
+    parse_classes(&html);
+    parse_css(&css);
 
     Ok(())
 }
